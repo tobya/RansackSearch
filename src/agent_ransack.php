f00591dd0c8803777ca9c104fd6e2ba4f5ebc0b6
Toby Allen <toby@toflidium.com>
2013-01-01 09:25:58 +0000

    Rearranged files for github

--- src/agent_ransack.php
+++ src/agent_ransack.php
@@ -1,14 +1,14 @@
 <?php
 /*************************************************************
-Copyright © 2012 Toby Allen (http://github.com/tobya)
+Copyright ï¿½ 2012 Toby Allen (http://github.com/tobya)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ï¿½Softwareï¿½), to deal in the Software without restriction, 
 including without limitation the rights to use, copy, modify, merge, publish, distribute, sub-license, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
 subject to the following conditions:
 
 The above copyright notice, and every other copyright notice found in this software, and all the attributions in every file, and this permission notice shall be included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. 
+THE SOFTWARE IS PROVIDED ï¿½AS ISï¿½, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. 
 IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ****************************************************************/
@@ -24,11 +24,11 @@ Description: Php Script to interface with AgentRansack.
 class phpAgentRansack
 {
     // property declarations
-	public $AgentRansack_Path = '';			//Where the AgentRansack.exe file lives.
+	public $AgentRansack_Path = '';			        //Where the AgentRansack.exe file lives.
 	public $OutputDirectory = 'c:\\temp\\';			//Where output file should be written to.
-	public $SearchSubFolders = true;	//Should the search include sub directories.
-	public $SearchDirectory = "";  //Root Directory to search - MUST BE SET BY USER.
-	public $SearchString = '';
+	public $SearchSubFolders = true;	          //Should the search include sub directories.
+	public $SearchDirectory = "";               //Root Directory to search - MUST BE SET BY USER.
+	public $SearchString = '';                  //File Match String
 	public $Current_Output_File = '';
 	public $FullCMD = '';
 	public $ResultArray = array();
@@ -48,6 +48,8 @@ class phpAgentRansack
      {
         if ($min > 2)
         {
+          //There is  a difference how later versions of php >5.3 handle output calls to
+          //exec. Essential to Wrap full cmd in quotes.
           $this->WrapInQuotes = false;
         }
      }
@@ -63,11 +65,9 @@ class phpAgentRansack
 
     // method declaration
     public function execute() {
-    echo 'in execute';
-		$Config = $this->createConfigArray();
-		return $this->execute_config($Config);
-		
-
+       echo 'in execute';
+		  $Config = $this->createConfigArray();
+		  return $this->execute_config($Config);
     }
 
     public function execute_config($ConfigArray)
